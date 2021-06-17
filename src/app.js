e6cb4e3be9b271b47f0ce4224b52f15b15653a8b
Mauro Wendel Rodrigues Costa <costa.mauro@eb.mil.br>
2021-06-17 13:57:40 -0300

    corrçeõea para api

--- src/app.js
+++ src/app.js
@@ -4,7 +4,14 @@ const routes = require('./routes');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+  app.use(cors());
+  next();
+});
+
 app.use(routes);
 
 app.listen(8004, () => {
